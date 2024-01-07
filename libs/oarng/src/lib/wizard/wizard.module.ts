90e2eb756f9cc377fbf2836ca65fdb1325fc18fe
chuanlin2018 <c.lin@prometheuscomputing.com>
2024-01-07 14:10:47 -0500

    Upgraded to Angular 16. Removed wizardpannel component since we don't use it.

--- libs/oarng/src/lib/wizard/wizard.module.ts
+++ libs/oarng/src/lib/wizard/wizard.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { SidebarModule } from 'ng-sidebar';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
 
-import { WizardPanelComponent } from './wizardpanel.component';
 import { PushingSidebarComponent } from './pushingsidebar.component';
 import { SlideoutColumnComponent } from './slideoutcol.component';
 
@@ -18,13 +17,11 @@ import { SlideoutColumnComponent } from './slideoutcol.component';
         SidebarModule.forRoot()
     ],
     declarations: [
-        WizardPanelComponent,
         PushingSidebarComponent,
         SlideoutColumnComponent
     ],
     providers: [ ],
     exports: [
-        WizardPanelComponent,
         PushingSidebarComponent,
         SlideoutColumnComponent
     ]
