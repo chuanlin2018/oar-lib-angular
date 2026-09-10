96e89128d2fc42c2833a69830a67d207340b8130
chuanlin2018 <c.lin@prometheuscomputing.com>
2026-09-10 14:48:35 -0400

    Use original NIST logo at the header bar. Adjusted other items accordingly.

--- libs/oarng/src/lib/frame/header/header-pub/header-pub.component.ts
+++ libs/oarng/src/lib/frame/header/header-pub/header-pub.component.ts
@@ -3,28 +3,31 @@ import { NistLogoComponent } from '../nist-logo/nist-logo.component';
 import { CommonModule } from '@angular/common';
 import { DOCUMENT } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faXmark, faHouse } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-header-pub',
-  standalone: true,
-  imports: [ NistLogoComponent, CommonModule, FontAwesomeModule ],
-  templateUrl: './header-pub.component.html',
-  styleUrls: ['./header-pub.component.css','../header.component.css']
+    selector: 'app-header-pub',
+    standalone: true,
+    imports: [NistLogoComponent, CommonModule, FontAwesomeModule],
+    templateUrl: './header-pub.component.html',
+    styleUrls: ['./header-pub.component.css', '../header.component.css'],
 })
 export class HeaderPubComponent {
-    title_line01: string = "MIDAS";
-    title_line02: string = "DATA PUBLISHING";
-    
+    title_line01: string = 'MIDAS';
+    title_line02: string = 'DATA PUBLISHING';
+    mobileMenuOpen = false;
+
+    faBars = faBars;
+    faXmark = faXmark;
     faHouse = faHouse;
 
-    @Input() appVersion: string = "1.0";
-    @Input() titleLn1: string = "MIDAS";
-    @Input() titleLn2: string = "DATA PUBLISHING";
-    @Input() homeButtonLink: string = "";
+    @Input() appVersion: string = '1.0';
+    @Input() titleLn1: string = 'MIDAS';
+    @Input() titleLn2: string = 'DATA PUBLISHING';
+    @Input() homeButtonLink: string = '';
 
-    constructor(@Inject(DOCUMENT) private document: Document) { }
-    
+    constructor(@Inject(DOCUMENT) private document: Document) {}
+
     ngOnInit() {
         this.title_line01 = this.titleLn1.toUpperCase();
         this.title_line02 = this.titleLn2.toUpperCase();
@@ -32,5 +35,13 @@ export class HeaderPubComponent {
 
     goHome() {
         this.document.location.href = this.homeButtonLink;
+    }
+
+    toggleMobileMenu(): void {
+        this.mobileMenuOpen = !this.mobileMenuOpen;
+    }
+
+    closeMobileMenu(): void {
+        this.mobileMenuOpen = false;
     }
 }
--- libs/oarng/src/lib/frame/header/nist-logo/nist-logo.component.ts
+++ libs/oarng/src/lib/frame/header/nist-logo/nist-logo.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'lib-nist-logo',
-  standalone: true,
-  imports: [],
-  templateUrl: './nist-logo.component.html',
-  styleUrls: ['./nist-logo.component.css']
+    selector: 'lib-nist-logo',
+    standalone: true,
+    imports: [],
+    templateUrl: './nist-logo.component.html',
+    styleUrls: ['./nist-logo.component.css'],
 })
 export class NistLogoComponent {
-  @Input() titleLn1: string = "MIDAS";
-  @Input() titleLn2: string = "DATA PUBLISHING";
+    @Input() titleLn1: string = 'MIDAS';
+    @Input() titleLn2: string = 'DATA PUBLISHING';
+    @Input() appVersion = '';
 }
